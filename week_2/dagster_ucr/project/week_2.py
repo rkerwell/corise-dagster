7644154c53c202761c0054242f4a24c73aa3e930
Ritesh Kerwell <57279059+rkerwell@users.noreply.github.com>
2022-08-27 23:33:38 -0400

    week_2_project

--- week_2/dagster_ucr/project/week_2.py
+++ week_2/dagster_ucr/project/week_2.py
@@ -4,27 +4,53 @@ from dagster import In, Nothing, Out, ResourceDefinition, graph, op
 from dagster_ucr.project.types import Aggregation, Stock
 from dagster_ucr.resources import mock_s3_resource, redis_resource, s3_resource
 
+# Use S3 resource and `get_data()` method to fetch rows from s3 bucket
+# Config provided in the job definition 
+@op(
+    required_resource_keys={"s3"},
+    config_schema={"s3_key": str},
+    out={"stocks": Out(dagster_type=List[Stock])},
+    tags={"kind": "s3"},
+    description="Get a list of stocks from an S3 file",
+)
+def get_s3_data(context):
+    s3_key = context.op_config["s3_key"]    # define s3 key
+    data = context.resources.s3.get_data(s3_key)    # fetch rows/data using key
+    output = list()
+    for row in data:
+        stock = Stock.from_list(row)
+        output.append(stock)
+    return output      
 
-@op
-def get_s3_data():
-    pass
+# from Week 1
+@op(
+    ins={"stocks" : In(dagster_type=List[Stock])},
+    out={"aggregation": Out(dagster_type=Aggregation)},
+    description="Given a list of stocks return the Aggregation with the greatest price",
+)
+def process_data(stocks):
+    highest_stock = max(stocks, key=lambda s:s.high)  # find max stock using `high` attribute of each element
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
 
 
-@op
-def process_data():
-    # Use your op from week 1
-    pass
+# Use Redis resource and `put_data()` method to load data as string only
+# Config provided in the job definition 
+@op(
+    required_resource_keys={"redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    tags={"kind":"redis"},
+    description="Upload an Aggregation to Redis",
+)
+def put_redis_data(context, aggregation):
+    context.resources.redis.put_data(str(aggregation.date), str(aggregation.high))
+    
 
-
-@op
-def put_redis_data():
-    pass
-
-
+# from Week 1 
 @graph
 def week_2_pipeline():
-    # Use your graph from week 1
-    pass
+    A = get_s3_data()
+    B = process_data(A)
+    C = put_redis_data(B)
 
 
 local = {
--- week_2/dagster_ucr/resources.py
+++ week_2/dagster_ucr/resources.py
@@ -91,13 +91,38 @@ def mock_s3_resource():
     return s3_mock
 
 
-@resource
-def s3_resource():
+### Week 2 ###
+# Client 1 = S3 resource
+@resource(
+    config_schema={
+        "bucket" : Field(str),
+        "access_key" : Field(str),
+        "secret_key" : Field(str),
+        "endpoint_url" : Field(str)
+    },
+    description="This resource defines a S3 client"
+)
+def s3_resource(context):
     """This resource defines a S3 client"""
-    pass
+    return S3(
+        bucket=context.resource_config["bucket"],
+        access_key=context.resource_config["access_key"],
+        secret_key=context.resource_config["secret_key"],
+        endpoint_url=context.resource_config["endpoint_url"]
+    )
 
 
-@resource
-def redis_resource():
+# Client 2 = Redis resource
+@resource(
+    config_schema={
+        "host" : Field(str),
+        "port" : Field(int)
+    },
+    description="This resource defines a Redis client"
+)
+def redis_resource(context):
     """This resource defines a Redis client"""
-    pass
+    return Redis(
+        host=context.resource_config["host"],
+        port=context.resource_config["port"]
+    )
