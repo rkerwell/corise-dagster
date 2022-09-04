2656551e8dd129452de4bcdb55e98ab21f62b8cb
Ritesh Kerwell <57279059+rkerwell@users.noreply.github.com>
2022-09-04 17:37:38 -0400

    week_3

--- week_3/project/week_3.py
+++ week_3/project/week_3.py
@@ -19,28 +19,52 @@ from project.sensors import get_s3_keys
 from project.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    # Use your ops from week 2
-    pass
+# from WEEK 2
+@op(
+    required_resource_keys={"s3"},
+    config_schema={"s3_key": str},
+    out={"stocks": Out(dagster_type=List[Stock])},
+    tags={"kind": "s3"},
+    description="Get a list of stocks from an S3 file",
+)
+def get_s3_data(context):
+    s3_key = context.op_config["s3_key"]    
+    data = context.resources.s3.get_data(s3_key)    
+    output = list()
+    for row in data:
+        stock = Stock.from_list(row)
+        output.append(stock)
+    return output 
 
 
-@op
-def process_data():
-    # Use your ops from week 2
-    pass
+# from WEEK 1
+@op(
+    ins={"stocks" : In(dagster_type=List[Stock])},
+    out={"aggregation": Out(dagster_type=Aggregation)},
+    description="Given a list of stocks return the Aggregation with the greatest price",
+)
+def process_data(stocks):
+    highest_stock = max(stocks, key=lambda s:s.high) 
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
 
 
-@op
-def put_redis_data():
-    # Use your ops from week 2
-    pass
+# from WEEK 2
+@op(
+    required_resource_keys={"redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    tags={"kind":"redis"},
+    description="Upload an Aggregation to Redis",
+)
+def put_redis_data(context, aggregation):
+    context.resources.redis.put_data(str(aggregation.date), str(aggregation.high))
 
 
+# from WEEK 1 
 @graph
 def week_3_pipeline():
-    # Use your graph from week 2
-    pass
+    A = get_s3_data()
+    B = process_data(A)
+    C = put_redis_data(B)
 
 
 local = {
@@ -69,8 +93,29 @@ docker = {
 }
 
 
-def docker_config():
-    pass
+# Static Partitioning by month , format of ingested files `prefix/stock_{month}.csv`
+KEYS = [str(_) for _ in range(1,11)]
+@static_partitioned_config(partition_keys=KEYS)
+def docker_config(partition_key: str):
+    return {
+            "resources": {
+                    "s3": {
+                        "config": {
+                            "bucket": "dagster",
+                            "access_key": "test",
+                            "secret_key": "test",
+                            "endpoint_url": "http://localstack:4566",
+                        }
+                    },
+                    "redis": {
+                        "config": {
+                            "host": "redis",
+                            "port": 6379,
+                        }
+                    },
+                },
+            "ops": {"get_s3_data": {"config": {"s3_key": f"prefix/stock_{partition_key}.csv"}}},
+            }
 
 
 local_week_3_pipeline = week_3_pipeline.to_job(
@@ -89,14 +134,52 @@ docker_week_3_pipeline = week_3_pipeline.to_job(
         "s3": s3_resource,
         "redis": redis_resource,
     },
+    op_retry_policy=RetryPolicy(max_retries=10, delay=1)        # retry policy for Redis
 )
 
 
-local_week_3_schedule = None  # Add your schedule
+#  run `local_week_3_pipeline` every 15 mins
+local_week_3_schedule = ScheduleDefinition(job=local_week_3_pipeline, cron_schedule="*/15 * * * *")
 
-docker_week_3_schedule = None  # Add your schedule
+# run `docker_week_3_pipeline` every hour
+docker_week_3_schedule = ScheduleDefinition(job=docker_week_3_pipeline, cron_schedule="0 * * * *")
 
 
-@sensor
-def docker_week_3_sensor():
-    pass
+# check `dagster` bucket with prefix `prefix` 
+# helper fn `get_s3_keys()` to check for new files
+@sensor(
+    job=docker_week_3_pipeline,
+    minimum_interval_seconds=30
+)
+def docker_week_3_sensor(context):
+    new_files = get_s3_keys(
+        bucket="dagster",
+        prefix="prefix",
+        endpoint_url="http://host.docker.internal:4566/"
+    )
+    if not new_files:
+        yield SkipReason("No new s3 files found in bucket.")
+        return
+    for new_file in new_files:
+        yield RunRequest(
+            run_key=new_file,
+            run_config={    # use Dockerized config from @72
+                "resources": { 
+                    "s3": {
+                        "config": {
+                            "bucket": "dagster",
+                            "access_key": "test",
+                            "secret_key": "test",
+                            "endpoint_url": "http://host.docker.internal:4566" 
+                        }
+                    },
+                    "redis": {
+                        "config": {
+                            "host": "redis",
+                            "port": 6379,
+                        }
+                    }
+            },
+            "ops": {"get_s3_data": {"config": {"s3_key": new_file}}},         # run op on each new_file
+            }
+        )
